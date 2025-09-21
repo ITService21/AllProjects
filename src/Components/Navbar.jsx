bac823dc98a57a4945b2efbeda3bd641400860ee
ITService21 <ssinghd049@gmail.com>
2025-09-21 23:20:07 +0530

    done

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -68,7 +68,7 @@ function Navbar() {
         transition={{ duration: 0.6, ease: [0.68, -0.55, 0.265, 1.55] }}
       >
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex items-center justify-between h-20">
+          <div className="flex items-center justify-between h-14 sm:h-16 md:h-18 lg:h-20">
             
             {/* Logo Section */}
             <motion.div 
